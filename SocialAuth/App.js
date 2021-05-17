0147ea50fa90eb37392081fa549366e193147058
AKAaayush <shresthaaayush81@gmail.com>
2021-05-17 15:09:44 +0545

    initial commit

---
+++ SocialAuth/App.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  LoginButton,
+  AccessToken,
+  GraphRequestManager,
+  LoginManager,
+  GraphRequest,
+} from 'react-native-fbsdk-next';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Social Authentication</Text>
+      </View>
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            AccessToken.getCurrentAccessToken().then(data => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+        onLogoutFinished={() => console.log('logout.')}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default app;
